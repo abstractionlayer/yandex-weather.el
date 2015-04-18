3d1e44becabd75fa8b32ffcb87c7d8120bad009a
Whitesquall <whitesquall@gmx.com>
2015-04-18 14:00:30 +0300

    Load the file with data only one time for all tests.

--- tests/yandex-weather-tests.el
+++ tests/yandex-weather-tests.el
@@ -74,7 +74,16 @@ You can run ert manually or using makefile."
       yandex-weather-test-data-file
     (concat "tests/" yandex-weather-test-data-file)))
 
-(defun yandex-weather-get-data-fixture (body date format)
+(defun yandex-weather-get-test-data ()
+  "Return mocked test xml data."
+  (with-current-buffer
+      (find-file (yandex-weather-find-file-with-test-data))
+    (let ((data (xml-parse-region (point-min)
+                                  (point-max))))
+      (kill-buffer (current-buffer))
+      data)))
+
+(defun yandex-weather-get-data-fixture (body date format data)
   (unwind-protect
       (progn
         (let ((org-yandex-weather-display-icon-p nil)
@@ -82,12 +91,7 @@ You can run ert manually or using makefile."
               (org-yandex-weather-format format))
           (flet ((yandex-weather-get-data
                   (location expire-time)
-                  (with-current-buffer
-                      (find-file ( yandex-weather-find-file-with-test-data))
-                    (let ((data (xml-parse-region (point-min)
-                                                  (point-max))))
-                      (kill-buffer (current-buffer))
-                      data)))
+                  data)
 
                  (org-yandex-weather-check-interval
                   (date)
@@ -98,28 +102,31 @@ You can run ert manually or using makefile."
 (ert-deftest org-yandex-weather-test ()
   "Test the org entry with the weather for location."
   :tags '(yandex-weather)
+  (let ((data (yandex-weather-get-test-data)))
+    (yandex-weather-get-data-fixture
+     (lambda ()
+       (should
+        (string-equal (org-yandex-weather)
+                      "Москва:  облачно, [-1,2]°C, →4.7 746 93")))
+     (list 1 15 2015)
+     "%C: %i %c, [%l,%h]%s, %d%w %p %H"
+     data)
 
-  (yandex-weather-get-data-fixture
-   (lambda ()
-     (should
-      (string-equal (org-yandex-weather)
-                    "Москва:  облачно, [-1,2]°C, →4.7 746 93")))
-   (list 1 15 2015)
-   "%C: %i %c, [%l,%h]%s, %d%w %p %H")
+    (yandex-weather-get-data-fixture
+     (lambda ()
+       (should
+        (equal (org-yandex-weather) nil)))
+     (list 1 14 2015)
+     "%C: %i %c, [%l,%h]%s, %d%w %p %H"
+     data)
 
-  (yandex-weather-get-data-fixture
-   (lambda ()
-     (should
-      (equal (org-yandex-weather) nil)))
-   (list 1 14 2015)
-   "%C: %i %c, [%l,%h]%s, %d%w %p %H")
-
-  (yandex-weather-get-data-fixture
-   (lambda ()
-     (should
-      (equal (org-yandex-weather) nil)))
-   (list 1 25 2015)
-   "%C: %i %c, [%l,%h]%s, %d%w %p %H")
+    (yandex-weather-get-data-fixture
+     (lambda ()
+       (should
+        (equal (org-yandex-weather) nil)))
+     (list 1 25 2015)
+     "%C: %i %c, [%l,%h]%s, %d%w %p %H"
+     data))
   )
 
 
