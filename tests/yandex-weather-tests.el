dad94fd7c98d7977f09727bf2e3dbf0e1f3bd834
Whitesquall <whitesquall@gmx.com>
2015-05-17 11:39:18 +0300

    Added test for the average night temperature.

--- tests/yandex-weather-tests.el
+++ tests/yandex-weather-tests.el
@@ -293,4 +293,20 @@ You can run ert manually or using makefile."
       (yandex-weather-data->forecast-by-date data (list 1 14 2015)))))
    ))
 
+(ert-deftest org-yandex-weather-forecast->avg-night-temperature-test ()
+ "Test the average night temperature from the forecast data."
+ :tags '(yandex-weather)
+ (let ((data (yandex-weather-get-test-data)))
+   (should
+    (string-equal
+     (yandex-weather-forecast->avg-night-temperature
+      (yandex-weather-data->forecast-by-date data (list 1 15 2015)))
+     "-1"))
+
+   (should
+    (not
+     (yandex-weather-forecast->avg-night-temperature
+      (yandex-weather-data->forecast-by-date data (list 1 14 2015)))))
+   ))
+
 ;;; yandex-weather-tests.el ends here
